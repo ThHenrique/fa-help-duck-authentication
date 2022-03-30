9a103a6b51f8a4615c2fa6fdd47d87c48ed1ff36
thhenrique <henriquethiago298@gmail.com>
2022-03-30 13:46:15 -0300

    added swagger docs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import "reflect-metadata"
 import express from 'express';
+import swaggerUi from "swagger-ui-express";
+
+import swaggerDocs from "./swagger.json"
 
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
 app.use(routes);
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));

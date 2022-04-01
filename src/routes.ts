ce0bd2783515374bc0416aefca94754576abb754
thhenrique <henriquethiago298@gmail.com>
2022-04-01 10:06:56 -0300

    created .env

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
-import { Router } from "express";
+import { Router, Request, Response } from 'express';
 
 import AuthController from "./app/controllers/AuthController";
 import authMiddleware from "./app/middlewares/authMiddlewares";
 
 const router = Router()
 
+router.get('/', (req: Request, res: Response) => {
+  res.statusCode = 200
+  return res.json({
+    "message": "The Bugger Duck API: Server Started OK"
+  })
+})
 router.post('/auth', AuthController.authenticate)
 router.post('/auth/authorization', authMiddleware)
 
